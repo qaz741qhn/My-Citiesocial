294fd6fe523a07a0179aab2ac8901d4ae9e321ad
qaz741qhn <qaz741qhn@gmail.com>
2021-12-16 20:32:00 +0900

    add concern file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,9 @@
 class Product < ApplicationRecord
+  include CodeGenerator
+
   belongs_to :vendor
   validates :code, uniqueness: true
   validates :name, presence: true
   validates :list_price, :sell_price, numericality: { grater_than: 0, allow_nil: true }
 
-  extend FriendlyId
-  friendly_id :code_generator, use: :slugged, slug_column: :code
-
-  private
-
-  def code_generator
-    SecureRandom.hex(5)
-  end
 end
