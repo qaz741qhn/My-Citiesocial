cdd3c7d35c7f5296fe7071e1482cbd0b0df2c8bf
qaz741qhn <qaz741qhn@gmail.com>
2021-12-16 19:57:42 +0900

    add admin/product styling

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,47 @@
 class Admin::ProductsController < Admin::BaseController
 
+  before_action :set_product, only: [:edit, :update]
+
   def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "已成功新增商品"
+      redirect_to admin_products_path
+    else
+      flash.now[:notice] = "新增失敗"
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      flash[:notice] = "已成功更新商品"
+      redirect_to edit_admin_product_path(@product)
+    else
+      flash.now[:notice] = "更新失敗"
+      render 'edit'
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :vendor_id, :list_price, :sell_price, :on_sell)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,4 +2,6 @@ class Vendor < ApplicationRecord
   acts_as_paranoid
 
   validates :title, presence: true
+
+  scope :available, -> { where(online: true) }
 end
