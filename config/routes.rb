de2573bbe637fb78f45392b9b87d6e594b37201d
qaz741qhn <qaz741qhn@gmail.com>
2022-01-15 19:45:45 +0900

    cart order function

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :orders, except: [:new, :edit, :update, :destroy]
+
   namespace :admin do
     root 'products#index'
     resources :products, except: [:show]
